2e5c2ea5537e2faa48e10d064eb6d9ae95f25ec9
Akarsh Barar <barar.akarsh8@gmail.com>
2020-08-30 02:46:52 +0530

    added files and data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
-
+import Home from './Home';
+import Header from './Header';
 function App() {
   return (
     <div className="app">
-     <h1> Air BNBN clone</h1>
+     <Header/>
+     <Home/>
+
+        {/* Header */}
+
+        {/* Banner */}
+
+            {/* Search */}
+
+        {/* Card */}
+
+        {/* Footer */}
+
+      {/* SEARCH PAGE */}
+
+         {/* .... */}
     </div>
   );
 }
